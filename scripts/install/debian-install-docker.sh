1960571242ec29c5855201d992e7290c9d387b46
Robin SHAMSNEJAD <rsham@free.fr>
2025-12-29 16:05:24 +0100

    Add Docker install script for Debian

---
+++ scripts/install/debian-install-docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+apt update && apt full-upgrade || exit 1
+
+# Add Docker's official GPG key:
+apt install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+apt update && apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin vim fish tmux git
