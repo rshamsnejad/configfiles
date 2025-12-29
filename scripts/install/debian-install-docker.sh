a53b5ad5d68a65627eb7dae65a5962ab0bcd3a9a
Robin SHAMSNEJAD <rsham@free.fr>
2025-12-29 16:29:10 +0100

    Skip apt prompts

--- scripts/install/debian-install-docker.sh
+++ scripts/install/debian-install-docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-apt update && apt full-upgrade || exit 1
+apt update && apt full-upgrade -y || exit 1
 
 # Add Docker's official GPG key:
-apt install ca-certificates curl
+apt install -y ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -17,4 +17,4 @@ Components: stable
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
-apt update && apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin vim fish tmux git
+apt update && apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin vim fish tmux git
